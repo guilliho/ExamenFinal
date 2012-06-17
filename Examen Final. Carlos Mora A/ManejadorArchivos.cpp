b5d4432a515b51870c2105a15a407b83ed849d18
Ho <jose.cruz.mora@gmail.com>
2012-06-16 22:34:11 -0600

    Factory

--- Examen Final. Carlos Mora A/ManejadorArchivos.cpp
+++ Examen Final. Carlos Mora A/ManejadorArchivos.cpp
@@ -2,30 +2,93 @@
 
 bool ManejadorArchivos::ExisteDirectorio(string nombreDirectorio)
 {
-	return false;
+	if (access(nombreDirectorio.c_str(), 0) == 0)
+	{
+		struct stat status;
+		stat(nombreDirectorio.c_str(), &status);
+		if (status.st_mode & S_IFDIR)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return false;
+	}
 }
 
 bool ManejadorArchivos::ExisteArchivo(string nombreArchivo)
 {
-	return false;
+	struct stat status;
+	if (stat(nombreArchivo.c_str(), &status) != -1)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void ManejadorArchivos::CrearDirectorioData()
+{
+	if (!ManejadorArchivos::ExisteDirectorio("data"))
+	{
+		CreateDirectory("data", NULL);
+	}
 }
 
 void ManejadorArchivos::CrearDirectorio(string nombreDirectorio)
 {
-
+	string directorioCompleto = "data/" + nombreDirectorio;
+	CreateDirectory(directorioCompleto.c_str(), NULL);
 }
 
 void ManejadorArchivos::CrearArchivo(string nombreArchivo)
 {
-
+	if (!ManejadorArchivos::ExisteArchivo(nombreArchivo))
+	{
+		fstream stream;
+		stream.open(nombreArchivo.c_str(), ios::in | ios::out | ios::trunc);
+		stream.close();
+	}
 }
 
 void ManejadorArchivos::LimpiarArchivo(string nombreArchivo)
 {
-
+	if (ManejadorArchivos::ExisteArchivo(nombreArchivo))
+	{
+		fstream stream;
+		stream.open(nombreArchivo.c_str(), ios::out | ios::trunc);
+		stream.close();
+	}
 }
 
 void ManejadorArchivos::GuardarArchivo(string nombreArchivo)
 {
+	if (ManejadorArchivos::ExisteArchivo(nombreArchivo))
+	{
+		ManejadorArchivos::LimpiarArchivo(nombreArchivo);
+		fstream stream;
+		stream.open(nombreArchivo.c_str(), ios::in | ios::out);
+		// Logic for saving the list into the file
 
+		stream.close();
+	}
+}
+
+void ManejadorArchivos::LeerArchivo(string nombreArchivo)
+{
+	if (ManejadorArchivos::ExisteArchivo(nombreArchivo))
+	{
+		fstream stream;
+		stream.open(nombreArchivo.c_str(), ios::in | ios::out);
+		// Read file and return list
+
+		stream.close();
+	}
 }
--- Examen Final. Carlos Mora A/ManejadorArchivos.h
+++ Examen Final. Carlos Mora A/ManejadorArchivos.h
@@ -1,6 +1,11 @@
 #pragma once
 #include<iostream>
 #include<string>
+#include<io.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<windows.h>
+#include<fstream>
 
 using namespace std;
 
@@ -9,8 +14,10 @@ class ManejadorArchivos
 public:
 	static bool ExisteDirectorio(string nombreDirectorio);
 	static bool ExisteArchivo(string nombreArchivo);
+	static void CrearDirectorioData();
 	static void CrearDirectorio(string nombreDirectorio);
 	static void CrearArchivo(string nombreArchivo);
 	static void LimpiarArchivo(string nombreArchivo);
 	static void GuardarArchivo(string nombreArchivo);
+	static void LeerArchivo(string nombreArchivo);
 };
