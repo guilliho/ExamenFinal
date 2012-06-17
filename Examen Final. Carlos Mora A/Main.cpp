e68b4069ff2ea6464061c16612b22b118f7f69d0
Ho <jose.cruz.mora@gmail.com>
2012-06-17 04:00:58 -0600

    Added main

--- Examen Final. Carlos Mora A/Main.cpp
+++ Examen Final. Carlos Mora A/Main.cpp
@@ -1,9 +1,12 @@
 #pragma once
+#include"Controlador.h"
 #include<iostream>
 using namespace std;
 
 int main()
 {
+	Controlador* control = new Controlador();
+	control->inicio();
 	system("pause");
 	return 0;
 }
